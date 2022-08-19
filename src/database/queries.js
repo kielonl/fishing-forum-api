34eb826e1606343a4a9368db5b7708cf918436ce
kielon <mosadowski23@gmail.com>
2022-08-19 13:04:05 +0200

    added images to database

--- src/database/queries.js
+++ src/database/queries.js
@@ -6,7 +6,7 @@ const insertIntoDetailsQuery = (userInfo) => {
 };
 
 const insertPostQuery = (postInfo) => {
-  return `INSERT INTO public.post (title,content,author ) VALUES ('${postInfo.title}','${postInfo.content}','${postInfo.author}') RETURNING *`;
+  return `INSERT INTO public.post (title,content,author,image) VALUES ('${postInfo.title}','${postInfo.content}','${postInfo.author}','${postInfo.image}') RETURNING *`;
 };
 
 const appendUUIDToUser = (userUUID, detailsUUID) => {
